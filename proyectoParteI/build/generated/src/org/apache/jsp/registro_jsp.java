acc637dcb0a88c6bb262c645255bddde77628a66
Bove <ribohe94@gmail.com>
2015-05-22 03:36:57 -0600

    Elimino carpeta de ejemplo

---
+++ proyectoParteI/build/generated/src/org/apache/jsp/registro_jsp.java
@@ -0,0 +1,191 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class registro_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("<!DOCTYPE html>\n");
+      out.write("<html>\n");
+      out.write("    <head>\n");
+      out.write("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
+      out.write("        <title>Inicio</title>\n");
+      out.write("        <link rel=\"stylesheet\" type=\"text/css\" href=\"default.css\" />\n");
+      out.write("        <script type=\"text/javascript\" src=\"js/scripts.js\"></script>\n");
+      out.write("\n");
+      out.write("    </head>\n");
+      out.write("    <body>\n");
+      out.write("        <div id=\"wrapper\">\n");
+      out.write("            <header>\n");
+      out.write("                <section id=\"encabezado\">\n");
+      out.write("                    <h1>\n");
+      out.write("                        <a href=\"index.jsp\">\n");
+      out.write("                            <p id=\"pEncabezado\"><img src=\"images/Captura de pantalla de 2015-05-16 11_23_46.png\" id=\"img_logo\"/></p>\n");
+      out.write("                            <p id=\"pEncabezado\">SISMO WEB WORLD</p>\n");
+      out.write("                        </a>\n");
+      out.write("                    </h1>\n");
+      out.write("                </section>\n");
+      out.write("            </header>\n");
+      out.write("\n");
+      out.write("            <div id=\"contents\">\n");
+      out.write("\n");
+      out.write("                <section id=\"navegacion\">\n");
+      out.write("                    <article>\n");
+      out.write("                        <li><a href=\"index.jsp\"><p>INICIO</p></a></li>\n");
+      out.write("                    </article>\n");
+      out.write("                    <article>\n");
+      out.write("                        <li><a href=\"registro.jsp\"><p>REGISTRO</p></a></li>\n");
+      out.write("                    </article>\n");
+      out.write("                    <article>\n");
+      out.write("                        <li><a href=\"login.jsp\"><p>INGRESO</p></a></li>\n");
+      out.write("                    </article>\n");
+      out.write("                </section>\n");
+      out.write("\n");
+      out.write("                <section id=\"seccionRegistro\">\n");
+      out.write("                    <form id=\"formularioRegistro\" name=\"userform\" action=\"ServletControl\" method=\"GET\" onsubmit=\"prueba(document.forms.userform.password.value);\">\n");
+      out.write("                        <table>\n");
+      out.write("                            <tr><th colspan=\"2\"><h2>Ingrese los datos Solicitados</h2></th></tr>\n");
+      out.write("                            <tr>\n");
+      out.write("                                <td>Nombre:</td> \n");
+      out.write("                                <td><input id=\"nombre\" name=\"nombre\" type=\"text\" placeholder=\"Ingrese su nombre\" form=\"formularioRegistro\" required></td>\n");
+      out.write("                            </tr>\n");
+      out.write("                            <tr>\n");
+      out.write("                                <td>Primer Apellido:</td> \n");
+      out.write("                                <td><input id=\"apellido1\" name=\"apellido1\" type=\"text\" placeholder=\"Apellido 1\" form=\"formularioRegistro\" required></td>\n");
+      out.write("                            </tr>\n");
+      out.write("                            <tr>\n");
+      out.write("                                <td>Segundo Apellido:</td> \n");
+      out.write("                                <td><input id=\"apellido2\" name=\"apellido2\" type=\"text\" placeholder=\"Apellido 2\" form=\"formularioRegistro\" required></td>\n");
+      out.write("                            </tr>\n");
+      out.write("                            <tr>\n");
+      out.write("                                <td>E-mail:</td> \n");
+      out.write("                                <td><input id=\"email\" name=\"email\" type=\"email\" placeholder=\"xxx@xxx.xxx\"form=\"formularioRegistro\" required></td>\n");
+      out.write("                            </tr>\n");
+      out.write("                            <tr>\n");
+      out.write("                                <td>Teléfono:</td> \n");
+      out.write("                                <td><input id=\"telefono\" name=\"telefono\" type=\"tel\" placeholder=\"####-####\" form=\"formularioRegistro\" required></td>\n");
+      out.write("                            </tr>\n");
+      out.write("                            <tr>\n");
+      out.write("                                <td>Fecha Nacimiento:</td> \n");
+      out.write("                                <td><input id=\"nacimiento\" name=\"nacimiento\" type=\"date\" form=\"formularioRegistro\" required></td>\n");
+      out.write("                            </tr>\n");
+      out.write("                            <tr>\n");
+      out.write("                                <td>Contraseña:</td> \n");
+      out.write("                                <td><input id=\"password\" name=\"password\" type=\"password\" placeholder=\"Ingrese su contraseña\" \n");
+      out.write("                                           form=\"formularioRegistro\" required onkeyup=\"check_password(document.forms.userform.password.value)\">\n");
+      out.write("                                </td>\n");
+      out.write("                                <td>\n");
+      out.write("                                    <!-- pwd_bar is where the colored strength bar appears -->\n");
+      out.write("                                    <div id=\"pwd_bar\"></div>\n");
+      out.write("\n");
+      out.write("                                    <!-- pwd_meter is where the strength word appears, like \"weak\" -->\n");
+      out.write("                                    <span id=\"pwd_meter\"></span>\n");
+      out.write("\n");
+      out.write("                                    <!-- pwd_log is the section that debug information appears if you set DEBUG=1 above -->\n");
+      out.write("                                    <span id=\"pwd_log\"></span>\n");
+      out.write("                                </td>\n");
+      out.write("                            </tr>\n");
+      out.write("                            <tr>\n");
+      out.write("                                <td>Confirme Contraseña:</td> \n");
+      out.write("                                <td><input id=\"confirma\" type=\"password\" name=\"passwd1\" placeholder=\"Confirme su contraseña\" form=\"formularioRegistro\" required></td>\n");
+      out.write("                            </tr>\n");
+      out.write("                            <tr>\n");
+      out.write("                                <td colspan=\"2\" align=\"center\">\n");
+      out.write("                                    <input id=\"pwdStrength\" type=\"hidden\" name=\"pwdStrength\" value=\"0\">\n");
+      out.write("                                    <input type=\"submit\" value=\"Submit\">\n");
+      out.write("                                </td>\n");
+      out.write("                            </tr>\n");
+      out.write("                            <tr>\n");
+      out.write("                                <td colspan=\"2\"><input type=\"submit\" value=\"Aceptar\" form=\"formularioRegistro\"></td>\n");
+      out.write("                            </tr>\n");
+      out.write("                        </table>\n");
+      out.write("                    </form>\n");
+      out.write("                    <!--                    <form name=\"userform\" action=\"?\" method=\"get\" onsubmit=\"return submit_button(document.forms.userform.passwd1.value)\">\n");
+      out.write("                    \n");
+      out.write("                    \n");
+      out.write("                                            <table>\n");
+      out.write("                                                <tr>\n");
+      out.write("                    \n");
+      out.write("                                                    <td valign=\"top\">Password:</td>\n");
+      out.write("                    \n");
+      out.write("                                                    <td>\n");
+      out.write("                                                        <input type=\"password\" name=\"passwd1\" value=\"\" size=\"80\"\n");
+      out.write("                                                               maxlength=\"50\" onkeyup=\"check_password(document.forms.userform.passwd1.value)\">\n");
+      out.write("                    \n");
+      out.write("                                                         pwd_bar is where the colored strength bar appears \n");
+      out.write("                                                        <div id=\"pwd_bar\"></div>\n");
+      out.write("                    \n");
+      out.write("                                                         pwd_meter is where the strength word appears, like \"weak\" \n");
+      out.write("                                                        <span id=\"pwd_meter\"></span>\n");
+      out.write("                    \n");
+      out.write("                                                         pwd_log is the section that debug information appears if you set DEBUG=1 above \n");
+      out.write("                                                        <span id=\"pwd_log\"></span>\n");
+      out.write("                                                    </td>\n");
+      out.write("                                                </tr>\n");
+      out.write("                                                <tr>\n");
+      out.write("                                                    <td colspan=\"2\" align=\"center\">\n");
+      out.write("                                                        <input id=\"pwdStrength\" type=\"hidden\" name=\"pwdStrength\" value=\"0\">\n");
+      out.write("                                                        <input type=\"submit\" value=\"Submit\">\n");
+      out.write("                                                    </td>\n");
+      out.write("                                                </tr>\n");
+      out.write("                                            </table>    \n");
+      out.write("                    \n");
+      out.write("                    \n");
+      out.write("                                        </form>-->\n");
+      out.write("                </section>\n");
+      out.write("\n");
+      out.write("            </div>\n");
+      out.write("            <footer></footer>\n");
+      out.write("        </div>\n");
+      out.write("    </body>\n");
+      out.write("</html>\n");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
